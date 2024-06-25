0e11b913ab6e1234d9ac078728943f16ca73d40f
袵幽霊 <g0rgh0st@localhost.localdomain>
2024-06-25 16:44:04 +0100

    1n1t_react_refact0r

--- nfts.js
+++
@@ -1,7 +0,0 @@
-const options = {method: 'GET', headers: {accept: 'application/json', 'x-api-key':"563825e2a73549959ea1857726e8f726"}};
-
-fetch('https://api.opensea.io/api/v2/collections/collection_slug', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './c0mp0nents/Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
