23d86de82f81e1dabaf152790aa74659edecf846
袵幽霊 <g0rgh0st@localhost.localdomain>
2024-06-30 16:25:52 +0100

    v0.1.2

--- src/c0mp0nents/H0m3/H0m3.js
+++ src/c0mp0nents/H0m3/H0m3.js
@@ -2,7 +2,6 @@ import React from 'react';
 import sk3tch_H0m3 from './sk3tch_H0m3';
 import { ReactP5Wrapper } from 'react-p5-wrapper';
 import avatar from './i_h0m3/avatar.png';
-import s1g1l from './i_h0m3/s1g1l.png';
 import p1n from './i_h0m3/ic0ns/l0ca1_p1n.png';
 import c0ntact_data from './c0ntact_data';
 import './H0m3.css';
@@ -14,17 +13,25 @@ export default function H0m3() {
       <ReactP5Wrapper sketch={sk3tch_H0m3}/>
       <div id='c0nta1n3r'>
         <section id='whoami'>
-          <img className='s1g1l' src={avatar} alt=''/>
-          <img className='s1g1l' src={s1g1l} alt=''/>
-          <strong>$ whoami</strong>
-          <br/>
-          H1, my nam3 1s what?
-          My nam3 1s wh0?
-          My nam3 1s chka chka <strong id='us3rnam3'>G0rGh0st</strong>!
-          We1c0m3 t0 my i11 c0rn3r 0f th3 w3b!
-          <br/><br/>
-          <img id='l0cat10n' src={p1n} alt=''/>
-          0.0.0.0
+          <img id='avatar' src={avatar} alt=''/>
+          <div id='s1g1l'>
+            <p>
+              <strong>$ whoami</strong>
+              <br/><br/>
+              H1, my nam3 1s what?
+              My nam3 1s wh0?
+              My nam3 1s chka chka <strong id='us3rnam3'>G0rGh0$t</strong>!
+              We1c0m3 t0 my i11 c0rn3r 0f th3 w3b!
+              <br/><br/>
+              1'm a $3cur1ty r3$3arch3r & d3v310p3r 0f un1ic3n$3d $0ftwar3.
+              <br/><br/>
+              Curr3nt1y 1nt0 mak1ng c001 w3b$1t3$ f0r art1$t$ & cr3at0r$,
+              f331 fr33 t0 r3ach 0ut @ <strong>g0rgh0st@protonmail.com</strong>
+              <br/><br/>
+              <img id='l0cat10n' src={p1n} alt=''/>
+              0.0.0.0
+            </p>
+          </div>
         </section>
         <div id='chi1d'>
           <section id='c0ntact'>
--- src/c0mp0nents/H0m3/c0ntact_data.js
+++ src/c0mp0nents/H0m3/c0ntact_data.js
@@ -1,7 +1,9 @@
 import Emai1 from './i_h0m3/ic0ns/c0ntact/emai1.png';
 import G1tHub from './i_h0m3/ic0ns/c0ntact/g1thub.png';
 import HTB from './i_h0m3/ic0ns/c0ntact/htb.png';
+import H1 from './i_h0m3/ic0ns/c0ntact/h1.png';
 import Op3nS3a from './i_h0m3/ic0ns/c0ntact/op3ns3a.svg';
+import X from './i_h0m3/ic0ns/c0ntact/x.png';
 import IG from './i_h0m3/ic0ns/c0ntact/ig.png';
 
 const c0ntact_data = [
@@ -18,8 +20,16 @@ const c0ntact_data = [
         src: HTB
     },
     {
+        href: 'https://hackerone.com/g0rgh0st',
+        src: H1
+    },
+    {
         href: 'https://opensea.io/G0rGh0st',
         src: Op3nS3a
+    },
+    {
+        href: 'https://x.com/G0rGh0st_',
+        src: X
     },
     {
         href: 'https://instagram.com/g0rgh0st_',
